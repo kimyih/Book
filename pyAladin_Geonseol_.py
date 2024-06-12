1f17bad8bc14182ebc41a537756564bc82743239
kimyih <163376151+kimyih@users.noreply.github.com>
2024-06-13 00:54:41 +0900

    Update pyAladin_Geonseol_.py

--- pyAladin_Geonseol_.py
+++ pyAladin_Geonseol_.py
@@ -16,8 +16,15 @@ filename = f"Aladin/Geonseol/Geonseol_{current_date}.json"
 
 # 웹드라이버 설치
 options = ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument("--headless")  # Headless 모드 추가
+options.add_argument("--no-sandbox")  # Sandbox 모드 비활성화
+options.add_argument("--disable-dev-shm-usage")  # /dev/shm 사용 비활성화
+options.add_argument("--disable-gpu")  # GPU 비활성화
+options.add_argument("--window-size=1920x1080")  # 윈도우 크기 설정
 service = ChromeService(executable_path=ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service, options=options)
+
 
 # URL 열기
 browser.get('https://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=All&SearchWord=%EC%A7%80%EA%B2%8C+%EA%B8%B0%EB%8A%A5%EC%82%AC')
